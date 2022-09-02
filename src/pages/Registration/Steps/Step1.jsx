74012c9244f3466616dd2b2c7774a048781b3900
kemo12 <dodkamel919@gmail.com>
2022-09-02 16:11:32 +0300

    step 1 regiester valdation done, axsios prepration done for handling requests

--- src/pages/Registration/Steps/Step1.jsx
+++ src/pages/Registration/Steps/Step1.jsx
@@ -1,16 +1,53 @@
 import React, { useState } from 'react';
 import 'react-phone-number-input/style.css';
 import { Button, Col, Container, Form, Row } from 'react-bootstrap';
-import PhoneInput from 'react-phone-number-input';
+import PhoneInput, { isValidPhoneNumber } from 'react-phone-number-input';
 import Footer from '../../../components/common/Footer';
 import './../../Common.css';
 import { Link, useNavigate } from 'react-router-dom';
 import Stepper from '../../../components/common/Stepper';
 import './Steps.css';
+import axios from 'axios';
 
 const Step1 = () => {
-  const [phoneNumber, setPhoneNumbe] = useState();
+  axios.defaults.headers = {
+    'x-api-key': 'pCe43DkKlV8PTAaBJsqdx63Qo2Hk7SJf7JM6LJFI',
+  };
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [validEmail, setValidEmail] = useState(false);
+  const [phoneError, setPhoneError] = useState(false);
+  const [email, setEmail] = useState('');
   const navigate = useNavigate();
+  const handlePhoneSubmit = (event) => {
+    event.preventDefault();
+    if (
+      phoneNumber === undefined ||
+      isValidPhoneNumber(phoneNumber) === false ||
+      phoneNumber == ''
+    ) {
+      setPhoneError(true);
+    } else {
+      setPhoneError(false);
+      //handleing request
+      axios
+        .post(
+          `https://ovwxgt25b8.execute-api.us-west-2.amazonaws.com/dev/registration?identityType=Phone&identityValue=${phoneNumber}`
+        )
+        .then((res) => {
+          console.log(res);
+          console.log(res.data);
+        });
+    }
+  };
+  const handleEmailSubmit = (event) => {
+    event.preventDefault();
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+    setValidEmail(true);
+  };
   return (
     <Container>
       <Row className="justify-content-center mt-5">
@@ -27,19 +64,43 @@ const Step1 = () => {
               You must click the Verify By Text or Verify by Email Button to
               receive a Verification Code and Move forward
             </p>
-            <Form>
+            <Form onSubmit={handlePhoneSubmit}>
               <Form.Label className="label">Phone</Form.Label>
               <PhoneInput
                 className="PhoneInput"
                 placeholder="(201) 555-0123"
                 value={phoneNumber}
-                onChange={setPhoneNumbe}
+                onChange={(e) => {
+                  setPhoneNumber(e);
+                  if (
+                    e === undefined ||
+                    isValidPhoneNumber(e) === false ||
+                    e == ''
+                  ) {
+                    setPhoneError(true);
+                  } else {
+                    setPhoneError(false);
+                  }
+                }}
                 defaultCountry="US"
               />
+              {phoneError && (
+                <p
+                  style={{
+                    marginTop: ' 0.25rem',
+                    fontSize: '.875em',
+                    color: '#dc3545',
+                  }}
+                >
+                  You must enter a valid Phone
+                </p>
+              )}
               <Button
+                type="submit"
                 className="CommonButton"
                 variant="secondary"
-                onClick={() => navigate('/registration/step1/verification')}
+                /*                 onClick={() => navigate('/registration/step1/verification')}
+                 */
               >
                 Verify by Text Message
               </Button>
@@ -52,17 +113,28 @@ const Step1 = () => {
                 SMS Privacy policy
               </Link>
             </p>
-            <Form>
+            <Form
+              noValidate
+              validated={validEmail}
+              onSubmit={handleEmailSubmit}
+            >
               <Form.Label className="label">Email Address</Form.Label>
               <Form.Control
+                required
                 className="hieght-50px"
                 type="email"
                 placeholder="name@example.com"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
               />
+              <Form.Control.Feedback type="invalid">
+                You must enter a valid email
+              </Form.Control.Feedback>
               <Button
+                type="submit"
                 className="CommonButton mb-3"
                 variant="secondary"
-                onClick={() => navigate('/registration/step1/verification')}
+                /* onClick={() => navigate('/registration/step1/verification')} */
               >
                 Verify by Email
               </Button>
